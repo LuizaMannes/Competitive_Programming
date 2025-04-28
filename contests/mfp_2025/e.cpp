e75b7b8ca706ca1c5d2ceeb2c6bada5eace3d008
Luiza <luiza@MacBook-Pro-de-mac15pro-vas.local>
2025-04-28 15:57:56 -0300

    Vou terminar depois

--- contests/mfp_2025/e.cpp
+++ contests/mfp_2025/e.cpp
@@ -6,9 +6,6 @@ using namespace std;
 #define endl '\n'
 
 const int maxn = 105;
-vector<vector<int>> ar(maxn, vector<int> (maxn));
-int n, m;
-vector<pair<int,int>> mv = {{-1, 0}, {0, -1}};
 
 bool pode(int i, int j){
     return i < n && j < m && i >=0 && j >= 0;
@@ -24,25 +21,6 @@ void solve() {
         }
     }
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            bool igual = 0;
-            for(auto [x, y]: mv){
-                if(pode(x + i, y + j)){
-                    if(ar[x + i][y + j] == ar[i][j]) igual = 1;
-                }
-            }
-            if(igual) ar[i][j]--;
-        }
-    }
-
-    cout << endl;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cout << ar[i][j] << " ";
-        }
-        cout << endl;
-    }
 }
 
 signed main() {
