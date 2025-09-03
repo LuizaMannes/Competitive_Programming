4906cb7cd3c740056a045293c8528adc9235c56b
Luiza <luiza@MacBook-Pro-de-mac15pro-vas.local>
2025-09-03 17:09:56 -0300

    acabei tree algorithms :)

--- CSES_problems/tree_algorithms/counting_paths.cpp
+++ CSES_problems/tree_algorithms/counting_paths.cpp
@@ -5,27 +5,37 @@ using namespace std;
 #define int long long
 #define endl '\n'
 
-const int mx = 2e5 + 5;
+const int mx = 2e5 + 5, LG = 20;
 
-vector<int> adj[mx], tin(mx), tout(mx), res(mx), pai(mx);
-int T = 0;
+vector<int> adj[mx];
+int up[mx][LG], tin[mx], tout[mx], sum[mx], t = 0;
 
-void dfs(int u, int p = -1){
-    tin[u] = ++T;
-    for(auto v: adj[u]){
-        if(v != p){
-            pai[v] = u;
+void dfs(int u, int p = -1) {
+    tin[u] = t++;
+    for (int i = 0; i < LG - 1; i++) up[u][i + 1] = up[up[u][i]][i];
+    for (int v : adj[u])
+        if (v != p) {
+            up[v][0] = u;
             dfs(v, u);
         }
-    }
-    tout[u] = T;
+    tout[u] = t;
 }
 
-void ans(int u, int p = -1){
+bool ancestor(int u, int v) { return tin[u] <= tin[v] && tout[u] >= tout[v]; }
+
+int lca(int u, int v) {
+    if (ancestor(u, v)) return u;
+    if (ancestor(v, u)) return v;
+    for (int i = LG - 1; i >= 0; i--)
+        if (!ancestor(up[u][i], v)) u = up[u][i];
+    return up[u][0];
+}
+
+void res(int u, int p){
     for(auto v: adj[u]){
         if(v != p){
-            res[v] += res[u];
-            ans(v, u);
+            res(v, u);
+            sum[u] += sum[v];
         }
     }
 }
@@ -39,24 +49,25 @@ void solve() {
         adj[b].push_back(a);
     }
 
+    up[1][0] = 1;
     dfs(1);
 
     for(int i = 0; i < m; i++){
         int a, b; cin >> a >> b;
-        res[a]++;
-
-        for(auto v: adj[a]){
-            if(tout[v] < tout[b]) res[v]--;
-        }
-
-        for(auto v: adj[b]){
-            if(v != pai[b]) res[v]--;
-        }
+        
+        int c = lca(a,b);
+        
+        sum[a]++;
+        sum[b]++;
+        sum[c]--;
+        if(c != 1) sum[up[c][0]]--;
     }
 
-    ans(1);
-
-    for(int i = 1; i <= n; i++) cout << res[i] << " ";
+    res(1, 0);
+    
+    for(int i = 1; i <= n; i++){
+        cout << sum[i] << " ";
+    }
     cout << endl;
 }
 
